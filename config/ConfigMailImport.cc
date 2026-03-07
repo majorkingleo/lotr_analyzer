3711bf857d7b845d6c5e88495b2ca24c7aee98d1
Martin Oberzalek <kingleo@gmx.at>
2026-03-07 20:24:55 +0100

    removed unused config vars

--- config/ConfigMailImport.cc
+++ config/ConfigMailImport.cc
@@ -9,13 +9,9 @@ Configfile2::SectionKey<ConfigSectionMailImport> ConfigSectionMailImport::KEY( "
 
 ConfigSectionMailImport::ConfigSectionMailImport( const std::string & name_, Configfile2 *config_file_ )
 : Section( name_, config_file_ ),
-  ImportDirectory( "ImportDirectory", "uncompressed" ),
-  mail2text( "mail2text", "mail2text.sh" ),
-  reformime( "reformime", "reformime" )
+  ImportDirectory( "ImportDirectory", "cur" )
 {
 	registerValue( &ImportDirectory );
-	registerValue( &mail2text );
-	registerValue( &reformime );
 }
 
 void ConfigSectionMailImport::registerSection( Configfile2 *config_file )
--- config/ConfigMailImport.h
+++ config/ConfigMailImport.h
@@ -8,8 +8,6 @@ public:
 	static Configfile2::SectionKey<ConfigSectionMailImport> KEY;
 
 	CONFIG_SIMPLE_DECLARE_STR( ImportDirectory );
-	CONFIG_SIMPLE_DECLARE_STR( mail2text );
-	CONFIG_SIMPLE_DECLARE_STR( reformime );
 
 public:
 	ConfigSectionMailImport( const std::string & name_, Configfile2 *config_file_ );
